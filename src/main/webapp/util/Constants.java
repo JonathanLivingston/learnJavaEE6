9c31f03a92d1358bfc42612b0e60a3de21c6a092
Sergey Kupriyanov <Velzedor@yandex.ru>
2013-10-12 15:00:03 +0400

    simple programmed tag added

--- src/main/webapp/util/Constants.java
+++ src/main/webapp/util/Constants.java
@@ -8,6 +8,12 @@ public final class Constants {
 
 	public static final String HTML_TAG_P_OPEN = "<p>";
 	public static final String HTML_TAG_P_CLOSE = "</p>";
+	public static final String HTML_TAG_TR_OPEN = "<tr>";
+	public static final String HTML_TAG_TR_CLOSE = "</tr>";
+	public static final String HTML_TAG_TD_OPEN = "<td>";
+	public static final String HTML_TAG_TD_CLOSE = "</td>";
+	public static final String HTML_TAG_B_OPEN = "<b>";
+	public static final String HTML_TAG_B_CLOSE = "</b>";
 	public static final String HTML_TAG_BR = "<br/>";
 
 }
---
+++ src/main/webapp/util/tag/LabeldTextField.java
@@ -0,0 +1,53 @@
+package main.webapp.util.tag;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import static main.webapp.util.Constants.*;
+
+public class LabeldTextField extends SimpleTagSupport {
+
+	private String label;
+	private String value = "";
+	private String name;
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		JspContext jspContext = getJspContext();
+		JspWriter writer = jspContext.getOut();
+		writer.append(HTML_TAG_TR_OPEN).append(HTML_TAG_TD_OPEN)
+				.append(HTML_TAG_B_OPEN).append(label).append(HTML_TAG_B_CLOSE)
+				.append(HTML_TAG_TD_CLOSE).append(HTML_TAG_TD_OPEN)
+				.append("<input type=\"text\" name=\">").append(name)
+				.append("\" value=\"").append(value).append("\"/>")
+				.append(HTML_TAG_TD_CLOSE).append(HTML_TAG_TR_CLOSE);
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
